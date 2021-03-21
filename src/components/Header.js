7534059a9ac1bafc3b4ea9d5a34b9fcc299bb120
Bohdan Kucheriavyi <kucheriavyi.w32@gmail.com>
2021-03-21 22:05:35 +0200

    Add new components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
-// import useSignedInUser from 'hooks/useSignedInUser';
+import useSignedInUser from 'hooks/useSignedInUser';
+import ProfileTile from 'components/ProfileTile';
 
 const Header = ({ title }) => {
-  // const signedInUser = useSignedInUser();
+  const signedInUser = useSignedInUser();
+
+  useEffect(() => {
+    console.info(signedInUser);
+  }, [signedInUser]);
 
   return (
     <AppBar position={'fixed'}>
       <Toolbar>
-        <Typography variant={'h6'}>{title ? title : '404'}</Typography>
+        <Typography variant={'h6'}>{title}</Typography>
+        {signedInUser ? <ProfileTile photo={signedInUser.photoURL}>{signedInUser.displayName}</ProfileTile> : null}
       </Toolbar>
     </AppBar>
   );
